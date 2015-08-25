bab1cceee9e83f928dcb019a78edf1395bafeedc
radicalevan <radicalevan@gmail.com>
2015-08-24 20:34:27 -0700

    String things

--- stringNonsense/main.m
+++ stringNonsense/main.m
@@ -8,11 +8,22 @@
 
 #import <Foundation/Foundation.h>
 
+NSString * canadianize(NSString * string) ;
+NSNumber * numberize(NSString * string);
+NSString * respond(NSString * string);
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
        
         // 255 unit long array of characters
         char inputChars[255];
+        
+        char choice[255];
+        
+        NSLog(@"Enter the number of the thing you want to do: \n 1. Uppercase \n 2. Lowercase \n 3. Numberize \n 4. Canadainize \n 5. Respond \n 6. De-Space-It");
+        fgets(choice, 255, stdin);
+        NSString *choicestring = [NSString stringWithUTF8String:choice];
+        int choiceint = choicestring.intValue;
         
         printf("Input a string: ");
         // limit input to max 255 characters
@@ -26,48 +37,66 @@ int main(int argc, const char * argv[]) {
         NSRange rangedstring = [badString rangeOfString:@"\n"];
         NSString *inputString = [badString substringToIndex: rangedstring.location];
         
-        // print NSString object
         NSLog(@"Input was: %@", inputString);
-        // make string uppercase
-        NSString *uppercaseString = [inputString uppercaseString];
-        NSLog(@"Uppercase string is %@", uppercaseString);
         
-        // make string lowercase
-        NSString *lowercaseString = [inputString lowercaseString];
-        NSLog(@"Lowercase string is %@", lowercaseString);
-        
-        //numberize
-        
-        NSString *numbered
-        
-        (NSNumber *)numberFromString:(NSString *)string
-        
-        
-        //canadianized
-        NSString *canadian = @", eh?";
-        NSString *canadianized = [inputString stringByAppendingString:canadian];
-        NSLog(@"Canadians say %@", canadianized);
-        
-        
-        //respond
-        NSString *questionMark = @"I don't know.";
-        NSString *exclamationMark = @"Whoa, calm down!";
-        
-        if ([inputString hasSuffix:@"?"]) {
-            NSString *combined = [NSString stringWithFormat:@"%@ %@", inputString, questionMark];
-            NSLog(@"%@", combined);
-            
-        } else if ([inputString hasSuffix:@"!"]) {
-            NSString *combined = [NSString stringWithFormat:@"%@ %@", inputString, exclamationMark];
-            NSLog(@"%@", combined);
+        switch (choiceint) {
+            case 1:
+                NSLog(@"Uppercase string is %@", [inputString uppercaseString]);
+                break;
+            case 2:
+                NSLog(@"Lowercase string is %@", [inputString lowercaseString]);
+                break;
+            case 3:
+                numberize(inputString);
+                break;
+            case 4:
+                canadianize(inputString);
+                break;
+            case 5:
+                respond(inputString);
+                break;
+            case 6:
+                NSLog(@"Nospacessocrazy: %@", [inputString stringByReplacingOccurrencesOfString:@" " withString:@"-"]);
+                break;
+            default:
+                NSLog(@"That's not going to work");
+                break;
         }
-    
-        
-        //de-space
-        NSString *deSpaced = [inputString stringByReplacingOccurrencesOfString:@" " withString:@"-"];
-        NSLog(@"Nospacessocrazy: %@", deSpaced);
-
-        
     }
     return 0;
+}
+
+NSString *respond(NSString * string) {
+NSString *questionMark = @"I don't know.";
+NSString *exclamationMark = @"Whoa, calm down!";
+    NSString *combined;
+    
+if ([string hasSuffix:@"?"]) {
+    combined = [NSString stringWithFormat:@"%@ %@", string, questionMark];
+    NSLog(@"%@", combined);
+    
+} else if ([string hasSuffix:@"!"]) {
+    combined = [NSString stringWithFormat:@"%@ %@", string, exclamationMark];
+    NSLog(@"%@", combined);
+}
+    return combined;
+}
+NSString * canadianize(NSString * string) {
+    NSString *canadian = @", eh?";
+    NSString *canadianized = [string stringByAppendingString:canadian];
+    NSLog(@"Canadians say %@", canadianized);
+    return canadianized;
+}
+
+NSNumber * numberize(NSString * string) {
+    NSNumberFormatter *numberformatter = [[NSNumberFormatter alloc] init];
+    NSNumber *number = [numberformatter numberFromString: string];
+    if  (!number) {
+        NSLog(@"%@ is not a number", string);
+    }
+    else if (number) {
+        NSLog(@"%@", number);
+    }
+    
+    return number;
 }
